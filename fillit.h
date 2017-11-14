a9094a8567893ccfc62572827c66bc501931c618
Dmytro BEZRUCHENKO <dbezruch@e3r4p2.unit.ua>
2017-11-14 17:22:42 +0200

    struct

--- fillit.h
+++ fillit.h
@@ -13,6 +13,19 @@
 #ifndef FILLIT_H
 # define FILLIT_H
 
+/* I straight   polyomino    1 4  1111
+ *4 1  1111
+ *      square  polyomino
+ * T   T-polyomino 2 3 111010
+ */
+
 int		check_file(char *name);
+
+typedef struct  s_shape
+{
+    char    rows;
+    char    cols;
+    char    *map;
+}               t_shape;
 
 #endif
